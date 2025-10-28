9b34a5aa4ef2bc225ea0876f67b8033dc7b4d145
Nicolaas <github@sunnysideup.co.nz>
2025-10-28 18:14:07 +1300

    ENH: set data-columns for predefined searches

--- src/Extensions/ModelAdminExtensionForSelections.php
+++ src/Extensions/ModelAdminExtensionForSelections.php
@@ -4,6 +4,7 @@ namespace Sunnysideup\Selections\Extensions;
 
 use SilverStripe\Core\Extension;
 use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\GridField\GridFieldDataColumns;
 use SilverStripe\View\Requirements;
 use Sunnysideup\Selections\Model\Selection;
 
@@ -45,18 +46,43 @@ JS;
 
     public function updateList(&$list)
     {
-        $owner = $this->getOwner();
-        $usepredefinedselection = $owner->getRequest()->getVar('usepredefinedselection');
-        if ($usepredefinedselection) {
-            $selection = Selection::get()->byID((int) $usepredefinedselection);
-            if ($selection && $selection->exists()) {
-                $list = $selection->getSelectionDataList();
-            }
+        $selection = $this->getUserPredefinedSelection();
+        if ($selection && $selection->exists()) {
+            $list = $selection->getSelectionDataList();
         }
     }
 
     protected function sanitiseClassNameHelper($class)
     {
         return str_replace('\\', '-', $class ?? '');
+    }
+
+    protected function getUserPredefinedSelection(): ?Selection
+    {
+        $owner = $this->getOwner();
+        $usepredefinedselection = $owner->getRequest()->getVar('usepredefinedselection');
+        if ($usepredefinedselection) {
+            $selection = Selection::get()->byID((int) $usepredefinedselection);
+            if ($selection && $selection->exists()) {
+                return $selection;
+            }
+        }
+        return null;
+    }
+
+    protected function updateGridField(&$field)
+    {
+        // @todo: this does not seem to work
+        $owner = $this->getOwner();
+        $selection = $this->getUserPredefinedSelection();
+        if ($selection && $selection->exists()) {
+            $displayFields = $selection->getSelectionDisplayFields();
+            if (!empty($displayFields)) {
+                $field
+                    ->getConfig()
+                    ->getComponentByType(GridFieldDataColumns::class)
+                    ->setDisplayFields($displayFields);
+            }
+        }
     }
 }
